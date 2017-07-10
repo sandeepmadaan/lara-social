9fd3dfb8e817a0df7d48a16c761bcd3d2e5e8332
sandeep-ucreate <sandeepkaur@ucreate.co.in>
2017-07-10 14:06:49 +0530

    first commit

---
+++ app/Http/Controllers/SocialAuthController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\User;
+use Socialite;
+use Illuminate\Http\Request;
+
+class SocialAuthController extends Controller
+{
+    //
+    /**
+     * Redirect the user to the OAuth Provider.
+     *
+     * @return Response
+     */
+    public function redirectToProvider($provider)
+    {
+        return Socialite::driver($provider)->redirect();
+    }
+
+    /**
+     * Obtain the user information from provider.  Check if the user already exists in our
+     * database by looking up their provider_id in the database.
+     * If the user exists, log them in. Otherwise, create a new user then log them in. After that 
+     * redirect them to the authenticated users homepage.
+     *
+     * @return Response
+     */
+    public function handleProviderCallback($provider)
+    {
+        $user = Socialite::driver($provider)->user();
+
+        $authUser = $this->findOrCreateUser($user, $provider);
+        Auth::login($authUser, true);
+        return redirect($this->redirectTo);
+    }
+
+    /**
+     * If a user has registered before using social auth, return the user
+     * else, create a new user object.
+     * @param  $user Socialite user object
+     * @param $provider Social auth provider
+     * @return  User
+     */
+    public function findOrCreateUser($user, $provider)
+    {
+       /* $authUser = User::where('social_accounts', $user->id)->first();
+        if ($authUser) {
+            return $authUser;
+        }*/
+        //echo "<pre>";print_r($user);exit;
+        $account_data = json_encode(array($provider => $user));
+        //echo "<pre>";print_r($user);exit;
+        //echo "<pre>";print_r($account_data);exit;
+        return User::create([
+            'name'     => $user->name,
+            'email'    => $user->email,
+            'password' => 'sjghds00',
+            'social_accounts' => $account_data
+        ]);
+    }
+}
